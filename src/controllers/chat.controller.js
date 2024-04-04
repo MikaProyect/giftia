44746adb586cd31fa9a5cf4d1dfb460543583282
Sunight01 <kevin.bmorillas@gmail.com>
2024-04-04 17:47:32 -0300

    GPT Prompt added

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -1,10 +1,13 @@
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
+import { arr1String, arr2String, arr3String, arr4String, arr5String } from '../BDTest/products.js';
 
 dotenv.config();
 
+const prompt = `Eres una aplicacion que sirve para la búsqueda de regalos, tu trabajo es recibir preferencias y encontrar regalos que coincidan con esas preferencias. Tienes que responder solo en contexto de los regalos. Te llamas GIFTIA. Te daré una base de datos con distintos productos que analizarás para recomendar regalos. Estos tienen un "tag", puedes relacionarlos con las preferencias que te escriban. Debes entregar una lista con productos que cumplan con la busqueda. De esta manera tienes que nombrar cada producto que has elegido: 'Nombre del producto, precio'. Tienes que adaptar el presupuesto en CLP en caso de que te lo mencionen, este es la Base de datos: ${arr1String}, ${arr2String}, ${arr3String}, ${arr4String}, ${arr5String}.`
+
 let conversation = [
-    { role: 'system', content: 'Eres un asombroso asistente, llamado Stelaris' },
+    { role: 'system', content: prompt },
 ]
 
 const openai = new OpenAI({
@@ -18,7 +21,7 @@ export const sendMessage = async (req, res) => {
 
     try {
         const response = await openai.chat.completions.create({
-            model: 'gpt-4',
+            model: 'gpt-4-0125-preview',
             messages: conversation,
             stop: null,
         });
