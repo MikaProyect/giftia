b9a9e482ce823be62cfbb3688030d803a67a2f19
Sunight01 <kevin.bmorillas@gmail.com>
2024-04-08 18:06:14 -0400

    GestProd done

---
+++ GiftiaFront/src/pages/adminPages/EditPages/index.jsx
@@ -0,0 +1,11 @@
+import { EditProducts } from "../../../components/EditProducts/EditProducts";
+
+function EditPage() {
+    return (
+        <>
+            <EditProducts idProd={test} />
+        </>
+    )
+}
+
+export default EditPage;
