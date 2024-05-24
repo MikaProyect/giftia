e410c9b1214db3e988f1bcfdbb446a0372b585ac
Karina <karinabravocanales@gmail.com>
2024-05-23 22:48:09 -0400

    ryc front listo

--- GiftiaFront/src/components/BarraProgreso/Barra.jsx
+++ GiftiaFront/src/components/BarraProgreso/Barra.jsx
@@ -2,30 +2,45 @@ import * as React from "react";
 import { SpinnerIcon } from "@iconicicons/react";
 import { ProgressBar } from "@lemonsqueezy/wedges";
 
-export function Barra() {
+export function Example() {
   const [value, setValue] = React.useState(0);
 
   React.useEffect(() => {
     const interval = setInterval(() => {
-      setValue((value) => (value + 1) % 101);
-    }, 150);
+      setValue((value) => {
+        if (value < 100) {
+          return value + 1;
+        } else {
+          
+          clearInterval(interval);
+          return value;
+        }
+      });
+      
+    }, 70);
 
+    // Limpiar el intervalo si el valor es 100
+    if (value >= 100) {
+      //clearInterval(interval);
+      window.location.href = "/chat";
+    }
     return () => clearInterval(interval);
-  }, []);
+  }, [value]); // Agregar 'value' como dependencia
+
   return (
+    <div className="flex items-center justify-center p-5" style={{marginTop:"15%"}}>
+
     <div className="inline-block w-full max-w-[400px]">
       <ProgressBar
         afterIndicator={<SpinnerIcon className="animate-spin" />}
-        helperText="Helper text"
-        color="green"
         indicator={value + "%"}
-        label="Label"
-        tooltip="A tooltip is a small box that appears when hovering over a UI element, providing additional information."
+        color='secondary'
         value={value}
         max={100}
       />
     </div>
+    </div>
   );
 }
 
-export default Barra
+export default Example;
--- GiftiaFront/tailwind.config.mjs
+++ GiftiaFront/tailwind.config.mjs
@@ -1,10 +1,10 @@
 import { wedgesTW } from "@lemonsqueezy/wedges";
 
-/ @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 const config = {
   content: [
-    './src//.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-    'node_modules/@lemonsqueezy/wedges/dist/**/.{js,ts,jsx,tsx}',
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    'node_modules/@lemonsqueezy/wedges/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {},
   darkMode: "class",
