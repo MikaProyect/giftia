e30790fef8648f9f6c521edfaf159e4443bf2474
1verah <f.verazapata@uandresbello.edu>
2024-06-22 18:57:20 -0400

    a

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,5 @@
 import { Router } from 'express'
-<<<<<<< HEAD
-import { loginController, logoutController, registerController} from '../controllers/auth.controller.js'
-=======
 import { loginController, logoutController, registerController } from '../controllers/auth.controller.js'
->>>>>>> 76a9b3fe2253ee0d20a25894f9f056a8e2de916e
 import { validateSchema } from '../middlewares/validator.middleware.js'
 import { loginSchema, registerSchema } from '../schemas/auth.schema.js'
 
@@ -15,8 +11,4 @@ router.post('/register', validateSchema(registerSchema), registerController)
 
 router.post('/logout', logoutController)
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 76a9b3fe2253ee0d20a25894f9f056a8e2de916e
 export default router
