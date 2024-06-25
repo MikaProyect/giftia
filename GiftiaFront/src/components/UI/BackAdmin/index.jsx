070898a286dd143f143cee0ed6feb979a3e301f0
Karina <karinabravocanales@gmail.com>
2024-06-24 20:30:12 -0400

    alertas bonitas

--- GiftiaFront/src/components/UI/BackAdmin/index.jsx
+++ GiftiaFront/src/components/UI/BackAdmin/index.jsx
@@ -20,7 +20,7 @@ const BackAdmin = () => {
       {isAdmin && (
         <a href='/adminPages/gest-prod'>
           <svg className="w-11 h-11 mb-2 text-black dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 12H4m12 0-4 4m4-4-4-4m3-4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2"/>
+            <path stroke="#CBA3FB" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 12H4m12 0-4 4m4-4-4-4m3-4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2"/>
           </svg>
         </a>
       )}
